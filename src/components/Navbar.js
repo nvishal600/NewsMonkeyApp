bd07a23bd027d5201bd0c23ca340581f75d903c9
Vishal Bhardwaj <77552964+nvishal600@users.noreply.github.com>
2022-04-12 13:12:16 +0530

    check

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
+  const hello = async()=> {
+    let url = 'https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=4fc0853a916e412190cbf44f5b6f8360';
+    let data = await fetch(url);
+    let parseData = await data.json(); 
+    console.log(parseData);
+  }
+  useEffect(()=>{
+    hello();
+  },[])
     return (
       <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
-            <Link className="navbar-brand" to="/NewsMonkeyApp">NewsMonkey</Link>
+            <Link className="navbar-brand" to="/">NewsMonkey</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
